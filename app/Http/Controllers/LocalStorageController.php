535b7a70231fa24c21f4db9db218571825192366
= <aymancoom3@gmail.com>
2023-11-11 10:06:29 +0200

    Done the Formatting

--- app/Http/Controllers/LocalStorageController.php
+++ app/Http/Controllers/LocalStorageController.php
@@ -28,7 +28,7 @@ class LocalStorageController extends Controller
             $aDocument  = new Documents();
             $fileName  = $aPdfFile->getClientOriginalName();
             // Remove dashes, spaces, and plus signs
-            $fileName = str_replace(["-", " ", "+", "@", "#", "$", "%", "^", "&", "*"], "", $fileName);
+            $fileName = str_replace(["-", "+", "@", "#", "$", "%", "^", "&", "*"], "", $fileName);
             // Remove all non-alphanumeric characters
             // $fileName = preg_replace("/[^a-zA-Z0-9]/", "", $fileName);
             $mimeParts  = explode('/', $aPdfFile->getMimeType());
