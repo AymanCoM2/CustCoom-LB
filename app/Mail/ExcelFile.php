930f4e509142a546bda5cec11c6237611ff73033
Ayman <ayman.salah@2coom.com>
2023-12-13 10:41:41 +0200

    Done the Backu For the LB Project With Mail For the Three Person

---
+++ app/Mail/ExcelFile.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Attachment;
+class ExcelFile extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Excel File',
+        );
+    }
+
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'backup',
+        );
+    }
+
+
+    public function attachments(): array
+    {
+        $path  = "/home/ubuntu/Downloads/";
+        $finalPath = $path . 'Backup-LB.xlsx';
+        return [
+            Attachment::fromPath($finalPath)
+                ->as('Backup-LB.xlsx')
+                ->withMime('application/xlsx')
+        ];
+    }
+}
